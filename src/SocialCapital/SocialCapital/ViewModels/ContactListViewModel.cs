a57511bffb78a90d4d641add85830a923eceff4a
= <=>
2015-08-30 20:43:19 +0300

    control extracted

---
+++ src/SocialCapital/SocialCapital/ViewModels/ContactListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SocialCapital
+{
+	public class ContactListViewModel
+	{
+		public ObservableCollection<Contact> Contacts { get; set; }
+			
+		public ContactListViewModel ()
+		{
+			Log.GetLogger ().Log ("==== ContactListViewModel constructor ===");
+			var manager = new ContactManager ();
+			var contacts = manager.GetContactListPreview ();
+
+			Contacts = new ObservableCollection<Contact> (contacts);
+		}
+	}
+}
+
---
+++ src/SocialCapital/SocialCapital/Views/Controls/TagList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SocialCapital.Views.Controls
+{
+	public partial class TagList : ContentView
+	{
+		public TagList ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
